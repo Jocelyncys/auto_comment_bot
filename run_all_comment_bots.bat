72ca65da4d3700fdece5303de6333b6e4dce5c28
Jocelyn <jocelyn@i-neighbour.com>
2025-04-11 18:13:37 +0800

    This is auto comment bot script

---
+++ run_all_comment_bots.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Running Brave YouTube comment bot
+"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\z_comment_bot_yt.py"
+
+echo Running Brave Facebook comment bot
+"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\z_comment_bot_fb.py"
+
+echo Running Brave Instagram comment bot
+"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\z_comment_bot_ig.py"
+
+::echo Running Instagram comment bot (Profile 5)...
+::"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Google\Chrome\User Data\Profile 5\z_comment_bot_igp5.py"
+
+::echo Running YouTube comment bot (Profile 6)...
+::"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Google\Chrome\User Data\Profile 6\z_comment_bot_ytp6.py"
+
+::echo Running Facebook comment bot (Profile 7)...
+::"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Google\Chrome\User Data\Profile 7\z_comment_bot_fbp7.py"
+
+::echo Running Xiaohongshu comment bot (Default Profile)...
+::"C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Jocelyn Cheong.DESKTOP-2UCUSM0\AppData\Local\Google\Chrome\User Data\Default\z_comment_bot_xhs.py"
+
+echo.
+echo ✅ All bots executed. Please check the console for success count.
+pause
